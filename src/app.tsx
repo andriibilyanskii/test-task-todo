7e153ca07dacfbc1306bed414f029162b3229828
andriibilyanskii <andriibilyanskii@gmail.com>
2024-03-11 02:52:43 +0200

    delete comments

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 
 import './app.scss'
-// import { useAppDispatch, useAppSelector } from './store/redux-hooks'
-// import { SELECTORS } from './store/selectors'
-// import { increment, incrementByAmount } from './store/data-reducer'
 
 import { Todo } from './pages'
 
 function App(): React.ReactElement {
-  // const data = useAppSelector(SELECTORS.getDataStore)
-  // const dispatch = useAppDispatch()
-
   return <Todo></Todo>
 }
 
